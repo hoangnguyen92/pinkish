fd51d6f478473e5448b242c022d71fbc529fbe23
Hoang Nguyen <Hoang Nguyen>
2017-01-23 22:51:22 +0200

    first commit

---
+++ src/main/java/com/pinkish/PinkishApplication.java
@@ -0,0 +1,12 @@
+package com.pinkish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkishApplication.class, args);
+	}
+}
