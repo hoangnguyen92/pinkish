e2a3851ae186de5f7fc1687ba2da2300eafacd8d
mm_hoang <hoang.nguyen@mentormate.com>
2017-02-07 00:57:06 +0200

    sale order buy

---
+++ src/main/java/com/pinkish/bean/ItemBean.java
@@ -0,0 +1,46 @@
+package com.pinkish.bean;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ItemBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private String category;
+	private Integer quantity;
+	private BigDecimal cost;
+	private BigDecimal price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public BigDecimal getCost() {
+		return cost;
+	}
+	public void setCost(BigDecimal cost) {
+		this.cost = cost;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+}
